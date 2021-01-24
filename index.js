f849353c19830c108dab383b6d02bd9241e1afda
Kuddleman <edl1155@gmail.com>
2021-01-24 08:32:25 -0800

    add users.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import usersRoutes from './routes/users.js'
 
 const app = express()
 const PORT = 5000
 
 app.use(bodyParser.json())
 
-app.get('/',(req, res)=>{
-    console.log('TEST')
+app.use('/users', usersRoutes)
 
-    res.send('Hello from Homepage')
-})
+app.get('/',(req, res)=> res.send('Hello from Homepage'))
+    
+
+    
+
 
 
 
