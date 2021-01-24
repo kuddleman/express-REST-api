93552ed520fa804e3f9a704f1ff141ae44c7ef61
Kuddleman <edl1155@gmail.com>
2021-01-24 09:11:55 -0800

    complete post route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid'
 
 const router = express.Router()
 
+const users = []
+
+
 router.get('/',(req, res)=>{
-    res.send('Hello')
+    //console.log(users)
+    res.send(users)
+})
+
+router.post('/', (req, res)=>{
+    //console.log(`POST ROUTE REACHED`)
+
+    const user = req.body
+    
+    users.push({ ...user, id: uuidv4() })
+
+    res.send(`User with the name ${user.firstName} added to the database!`)
 })
 
 export default router
