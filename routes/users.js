7ba22c1659435533111bc1e6e60056cb934c765c
Kuddleman <edl1155@gmail.com>
2021-01-25 08:37:23 -0800

    add patch route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid'
 
 const router = express.Router()
 
-const users = []
+let users = []
 
 
 router.get('/',(req, res)=>{
@@ -26,6 +26,27 @@ router.get('/:id', (req, res)=>{
 
     const foundUser = users.find( user => user.id === id )
     res.send(foundUser)
+})
+
+router.delete('/:id', (req, res)=>{
+    const { id } = req.params
+
+    users = users.filter( user => user.id !== id )
+    res.send(`User with id ${id} has been deleted from the database.`)
+})
+
+router.patch('/:id', (req, res)=>{
+    const { id } = req.params
+    const { firstName, lastName, age } = req.body
+
+    const user = users.find(user => user.id === id)
+   
+    if(firstName) user.firstName = firstName
+    if(lastName)  user.lastName = lastName
+    if(age)  user.age = age
+       
+    res.send(`User with the id of ${id} has been updated`)
+    
 })
 
 export default router
